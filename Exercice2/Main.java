3b04601afa34b0293a186a6d736efd0a0b50b99d
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-11-18 13:29:10 +0100

    update

--- Exercice2/Main.java
+++ Exercice2/Main.java
@@ -2,6 +2,11 @@ package TP6.Exercice2;
 
 public class Main {
     public static void main(String[] args) {
+        Developpeur dev=new Developpeur("BADRI", "Yesser", "yesserBadri@gmail.com", "28 408 778", 3000.0, "Java");
         
+        Manager man=new Manager("BOUIN", "Kamel", "bouinKamel@gmail.com", "53 524 234", 2500.0, "Moudir");
+
+        dev.Afficher();
+        man.Afficher();
     }
 }
